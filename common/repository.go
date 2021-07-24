84a413023ffa5476e344a7ab0ad539906264b78f
soyeon <sypark9646@gmail.com>
2021-07-24 09:25:50 +0900

    add: get all method & set as data type

--- common/repository.go
+++ common/repository.go
@@ -21,7 +21,7 @@ func connection() redis.Conn {
 	return c
 }
 
-func Get(key int64) ([]byte, error) {
+func GET(key int64) ([]byte, error) {
 	conn := connection()
 	defer conn.Close()
 
@@ -31,6 +31,29 @@ func Get(key int64) ([]byte, error) {
 		return data, fmt.Errorf("error getting key %s: %v", key, err)
 	}
 	return data, err
+}
+
+func GETALL(pattern string) ([]byte, error) {
+	conn := connection()
+	defer conn.Close()
+
+	iter := 0
+	var result []byte
+	var data string
+	for {
+		if arr, err := redis.Values(conn.Do("SCAN", iter, "MATCH", pattern)); err != nil {
+			panic(err)
+		} else {
+			iter, _ = redis.Int(arr[0], nil)
+			data, _ = redis.String(arr[1], nil)
+			result = append(result, data...)
+		}
+
+		if iter == 0 {
+			break
+		}
+	}
+	return result, nil
 }
 
 func UPDATE(key int64, value []byte) error {
@@ -68,18 +91,7 @@ func POST(value []byte) error {
 	return err
 }
 
-func Exists(key int64) (bool, error) {
-	conn := connection()
-	defer conn.Close()
-
-	ok, err := redis.Bool(conn.Do("EXISTS", key))
-	if err != nil {
-		return ok, fmt.Errorf("error checking if key %s exists: %v", key, err)
-	}
-	return ok, err
-}
-
-func Delete(key int64) error {
+func DELETE(key int64) error {
 	conn := connection()
 	defer conn.Close()
 
@@ -93,4 +105,36 @@ func Incr(counterKey string) (int64, error) {
 
 	key, err := redis.Int64(conn.Do("INCR", counterKey))
 	return key, err
+}
+
+func POSTSet(setName string, value string) error {
+	conn := connection()
+	defer conn.Close()
+
+	_, err := conn.Do("SADD", setName, value)
+	if err != nil {
+		v := string(value)
+		if len(v) > 15 {
+			v = v[0:12] + "..."
+		}
+		return fmt.Errorf("error setting key %s to %s: %v", setName, v, err)
+	}
+	return err
+}
+
+func DELETESet(setName string, value string) error {
+	conn := connection()
+	defer conn.Close()
+
+	_, err := conn.Do("SPOP", setName, value)
+	return err
+}
+
+func GETALLSet(setName string) ([]byte, error) {
+	conn := connection()
+	defer conn.Close()
+
+	var data []byte
+	data, err := redis.Bytes(conn.Do("SMEMBERS", setName))
+	return data, err
 }
